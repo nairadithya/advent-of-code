df465adaec1d2f54d1e85c80f4ab051c15185d27
Adithya Nair <adithyanair121@gmail.com>
2024-12-01 21:23:32 +0530

    Got part a down

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,6 +3,36 @@ use std::fs;
 const INPUT_FILE: &str = "input/01.txt";
 
 fn main() {
-    let input = fs::read_to_string(INPUT_FILE).expect("Your file doesn't seem to exist my friend.");
-    let parts = input.split(" ");
+    let input = fs::read_to_string(INPUT_FILE).expect("Got no clue, this ain't no file.");
+    let parts: Vec<&str> = input.split_whitespace().collect();
+
+    let mut list1: Vec<i32> = parts
+        .iter()
+        .enumerate()
+        .filter(|(index, _)| index % 2 == 1)
+        .map(|(_, element)| element.parse::<i32>().expect("This is not a number my guy"))
+        .collect();
+
+    let mut list2: Vec<i32> = parts
+        .iter()
+        .enumerate()
+        .filter(|(index, _)| index % 2 != 1)
+        .map(|(_, element)| element.parse::<i32>().expect("This is not a number my guy"))
+        .collect();
+
+    list1.sort();
+    list2.sort();
+
+    let mut distance_list = Vec::new();
+    for (i, _) in list1.iter().enumerate() {
+        distance_list.push((list1[i] - list2[i]).abs());
+    }
+
+    let sum: i32 = distance_list.iter().sum();
+
+    println!("{}", sum)
+}
+
+fn main() {
+
 }
