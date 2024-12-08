9ebf890bde84b00d9bf949699c2801f65757c9d7
Adithya Nair <adithyanair121@gmail.com>
2024-12-08 14:43:10 +0530

    I don't like it, but it's a solution I guess.

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,12 +1,12 @@
 use regex::Regex;
 use std::fs;
 
-const INPUT_FILE: &str = "input/03-test.txt";
+const INPUT_FILE: &str = "input/03.txt";
 
 fn part_a() {
     let input = fs::read_to_string(INPUT_FILE).expect("Got no clue, this ain't no file.");
 
-    let re: Regex = Regex::new(r"(mul\(\d+,\d+\))").unwrap();
+    let re: Regex = Regex::new(r"(mul\(\d+,\d+\))").expect("Huh, no matches here.");
 
     let numbers = re
         .find_iter(&input)
@@ -14,9 +14,7 @@ fn part_a() {
             m.as_str()
                 .trim_matches(|x: char| x.is_alphabetic() || x.is_ascii_punctuation())
                 .split(",")
-                .collect::<Vec<&str>>()
-                .iter()
-                .map(|x| x.parse::<i32>().unwrap())
+                .map(|x| x.parse::<i32>().expect("That's not a number"))
                 .product::<i32>()
         })
         .sum::<i32>();
@@ -27,11 +25,36 @@ fn part_a() {
 fn part_b() {
     let input = fs::read_to_string(INPUT_FILE).expect("Got no clue, this ain't no file.");
 
-    let re: Regex = Regex::new(r"(?<mul>mul\(\d+,\d+\))").expect("Huh, no arguments here");
+    let re: Regex = Regex::new(r"do\(\)|mul\(\d+,\d+\)|don't\(\)").expect("Huh, no matches here");
 
-    println!("Solution For Part B:")
+    let list: Vec<&str> = re
+        .find_iter(&input)
+        .map(|m| m.as_str())
+        .collect::<Vec<&str>>();
+
+    let mut flag = true;
+    let mut sum = 0;
+    println!("Solution For Part B: {:?}", list);
+    for item in list {
+        if item == "do()" {
+            flag = true;
+            continue;
+        } else if item == "don't()" {
+            flag = false;
+            continue;
+        } else if flag == true {
+            sum += item
+                .trim_matches(|x: char| x.is_alphabetic() || x.is_ascii_punctuation())
+                .split(",")
+                .map(|y| y.parse::<i32>().expect("That's not a number"))
+                .product::<i32>();
+        }
+    }
+
+    println!("Solution For Part B: {}", sum);
 }
 
 fn main() {
     part_a();
+    part_b();
 }
