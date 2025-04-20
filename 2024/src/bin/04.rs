014b888689fc09d9161d8520e53ce9d975809f6f
Adithya Nair <adithyanair121@gmail.com>
2025-04-20 06:47:29 +0530

    Moved everything to one folder

--- 2024/src/bin/04.rs
+++ 2024/src/bin/04.rs
@@ -4,9 +4,6 @@ const INPUT_FILE: &str = "input/04-test.txt";
 
 fn part_a() {
     let input = fs::read_to_string(INPUT_FILE).expect("Bro give me a real file");
-    let box_size = input.expect("No newlines here.");
-
-    println!("{}", box_size);
 }
 
 fn main() {
